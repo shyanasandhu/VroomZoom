5930f39c9fe5b7d6d822e68e6885c76c3a6d0c31
Shyana Sandhu <shyanas2003@gmail.com>
2025-10-21 13:52:43 -0500

    Added new scripts + scenes

--- Final Project/Assets/Scripts/CarMovement.cs
+++ Final Project/Assets/Scripts/CarMovement.cs
@@ -8,9 +8,50 @@ public class CarMovement : MonoBehaviour
         
     }
 
+    public float delay = 3;
+    public Transform camPosition;
+    Vector3 moveDir;
+
     // Update is called once per frame
     void Update()
     {
+
+        float h = 0f;
+        float v = 0f;
+
+        //moves right
+        if(Input.GetKey(KeyCode.D)){
+            //transform.localPosition += new Vector3(2,0,0) * Time.deltaTime;
+            h = 1f;
+        }
+        
+        //moves left
+        if(Input.GetKey(KeyCode.A)){
+            //transform.localPosition += new Vector3(-2,0,0) * Time.deltaTime;
+            h = -1f;
+        }
+
+        //moves forward
+        if(Input.GetKey(KeyCode.W)){
+            //transform.localPosition += new Vector3(0,0,2) * Time.deltaTime;
+            v = 1f;
+        }
+        
+        //moves backward
+        if(Input.GetKey(KeyCode.S)){
+            //transform.localPosition += new Vector3(0,0, -2) * Time.deltaTime;
+            v = -1f;
+        }
+
+        Vector3 mForward = camPosition.forward;
+        Vector3 mRight = camPosition.right;
+
+        mForward.y = 0f;
+        mRight.y = 0f;
+
+        moveDir = (mForward*v + mRight*h).normalized;
+
+        transform.position += moveDir*7f*Time.deltaTime;
         
     }
 }
