768d4a6836356cfc0c0954f3c3c82c187b33d4e1
Shyana Sandhu <shyanas2003@gmail.com>
2025-11-27 22:30:53 -0600

    World building

--- Final Project/Assets/Scripts/LapTime.cs
+++ Final Project/Assets/Scripts/LapTime.cs
@@ -3,41 +3,7 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 
-/*public class LapTime : MonoBehaviour
+public class LapTime : MonoBehaviour
 {
-    public GameObject SF;
-    public int count = 0;
-    public float delay = 5;
-    private bool startT = false;
-
-
-    public void OnTriggerEnter(Collider other){
-        if(other.CompareTag("SFLine")){
-            count += 1;
-            recordTime();
-
-            if(count == 5){
-                EventManager.onTimerStop();
-                StartCoroutine(RunEnd(delay));
-            }
-
-            if(!startT){
-                EventManager.onTimerStart();
-                startT = true;
-            }
-            
-
-
-        }
-    }
-
-    private void recordTime(){
-
-    }
-
-    IEnumerator RunEnd(float delay){
-        yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene("LapTimes");
-    }
+    
 }
-*/
